3544672d37170d3c579f5a57246b44c04bd6afcb
Otoniel Rodriguez-Perez <orodriguez40@valenciacollege.edu>
2025-01-22 18:44:17 -0500

    Version 1

--- src/Patron.java
+++ src/Patron.java
@@ -1,24 +1,9 @@
-import javax.swing.*;
-import java.awt.*;
+// 1. Patron Class: used to define what a kind of information a
+//patron will contain.
+//It will be called by the Librarian class to store, view, or remove patrons.
 
-public class CheckersGame extends JFrame {
+class Patron {
 
-    private CheckersBoard board;
 
-    public CheckersGame() {
-        setTitle("Checkers Game");
-        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        setSize(600, 600);
-        setResizable(false); // Prevent resizing for simpler drawing
-        setLayout(new BorderLayout());
-
-        board = new CheckersBoard();
-        add(board, BorderLayout.CENTER);
-
-        setVisible(true);
-    }
-
-    public static void main(String[] args) {
-        SwingUtilities.invokeLater(() -> new CheckersGame());
-    }
 }
+
