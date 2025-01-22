e4d88103dd1797eb9727f448a284962c2c64d2ee
Otoniel Rodriguez-Perez <orodriguez40@valenciacollege.edu>
2025-01-22 18:53:14 -0500

    Version 2

--- src/Patron.java
+++ src/Patron.java
@@ -4,6 +4,57 @@
 
 class Patron {
 
+    //Attributes
+    private int id;
+    private String name;
+    private String address;
+    private double amountOwed;
+
+    //Constructor
+    public Patron(int id, String name, String address, double amountOwed) {
+        this.id = id;
+        this.name = name;
+        this.address = address;
+        this.amountOwed = amountOwed;
+    }
+
+    //Getters
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public double getAmountOwed() {
+        return amountOwed;
+    }
+
+
+    //Setters
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+
+    public void setAmountOwed(double amountOwed) {
+        this.amountOwed = amountOwed;
+    }
+
 
 }
+
 
