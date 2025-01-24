bfd954e52f87dd431ed95602c1f6583bd1c15a28
Otoniel Rodriguez-Perez <orodriguez40@valenciacollege.edu>
2025-01-23 21:02:13 -0500

    Version 11

--- src/Patron.java
+++ src/Patron.java
@@ -1,5 +1,4 @@
-// 1. Patron Class: used to define what a kind of information a
-//patron will contain.
+// 1. Patron Class: used to define what a kind of information a patron will contain.
 //It will be called by the Librarian class to store, view, or remove patrons.
 
 class Patron {
